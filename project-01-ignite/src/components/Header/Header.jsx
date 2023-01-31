e75183fc768afb00758dc8d3981777d601440ada
Douglas Toledo <dwtoledo@outlook.com>
2023-01-31 16:58:26 -0300

    Refactor Header Component

--- project-01-ignite/src/components/Header/Header.jsx
+++ project-01-ignite/src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ import IgniteFeedIcon from '../../assets/icons/ignite-icon.svg'
 export function Header() {
   return (
     <header className={styles.headerContent}>
-      <img className={styles.headerImage} src={IgniteFeedIcon} alt="Ignite Feed Icon" />
-      <p className={styles.headerTitle}>Ignite Feed - @dwtoledo</p>
+      <img src={IgniteFeedIcon} alt="Ignite Feed Icon" />
+      <p>Ignite Feed - @dwtoledo</p>
     </header>
   )
 }
