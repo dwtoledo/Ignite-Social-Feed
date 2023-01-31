b5fe3c273fabf92ecca149d9607134f58a1218c7
Douglas Toledo <dwtoledo@outlook.com>
2023-01-31 12:56:42 -0300

    Create Initial Header Component
    Implement Global and Reset CSS on App

--- project-01-ignite/src/App.jsx
+++ project-01-ignite/src/App.jsx
@@ -1,7 +1,13 @@
-function App() {
+import { Header } from "./components/Header/Header"
+
+import './reset.css'
+import './global.css'
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
   )
 }
-
-export default App
---
+++ project-01-ignite/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export function Header() {
+  return (
+    <header className={styles.headerContent}>
+      <p className={styles.headerTitle}>Ignite Project 01 - @dwtoledo</p>
+    </header>
+  )
+}
