1cb9fdc6251bab0c608a2244bd793dc0c4da054b
Douglas Toledo <dwtoledo@outlook.com>
2023-02-07 09:24:00 -0300

    Visual Adjustments in Case of Small Screens

--- project-01-ignite/src/components/ProfileCard/ProfileCard.jsx
+++ project-01-ignite/src/components/ProfileCard/ProfileCard.jsx
@@ -10,7 +10,7 @@ export function ProfileCard() {
       <div className={styles.profileCardHeader}>
         <img
           className={styles.profileCardCover}
-          src="https://images.unsplash.com/photo-1517191434949-5e90cd67d2b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80"
+          src="https://images.unsplash.com/photo-1517191434949-5e90cd67d2b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
           alt="Profile Cover Image"
         />
 
@@ -20,7 +20,6 @@ export function ProfileCard() {
         />
         
       </div>
-
 
       <div className={styles.profileCardInfo}>
         <strong className={styles.profileCardTitle}>Mayk Brito</strong>
